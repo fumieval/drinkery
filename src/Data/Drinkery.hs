3a005dff53ce3a87ad5696b2d69fe916c5f5cb06
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-12-04 15:11:25 +0900

    export Patron

--- src/Data/Drinkery.hs
+++ src/Data/Drinkery.hs
@@ -2,8 +2,10 @@ module Data.Drinkery
     ( module Data.Drinkery.Class
     , module Data.Drinkery.Distiller
     , module Data.Drinkery.Tap
+    , Patron(..)
     )where
 
+import Data.Drinkery.Boozer
 import Data.Drinkery.Class
 import Data.Drinkery.Distiller
 import Data.Drinkery.Tap
