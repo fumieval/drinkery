1d7bef0d95aca358693af4f4fde5c74f3784577e
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-12-05 16:18:21 +0900

    fix benchmark

--- benchmarks/benchmark.hs
+++ benchmarks/benchmark.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE BangPatterns, LambdaCase #-}
-{-# OPTIONS -ddump-simpl -dsuppress-all #-}
 import qualified Data.Drinkery as D
 import qualified Data.Drinkery.Glass as D
 import Data.Functor.Identity
@@ -10,16 +8,16 @@ import Data.List
 import Data.Void
 
 drainD :: D.Distiller () (Maybe Int) IO () (Maybe a) -> IO ()
-drainD h = sourceD D.+& h D.$& D.sinkNull
+drainD h = sourceD D.+& h D.$&& D.sinkNull
 
 value :: Int
 value = 10000
 
 sourceD :: (Monoid r, Monad m) => D.Tap m r (Maybe Int)
-sourceD = D.recommended $ D.taste [1..value]
+sourceD = D.runSommelier $ D.taste [1..value]
 
 main = defaultMain
   [ bgroup "scan"
-      [ bench "drnkery" $ whnfIO $ drainD (D.scanningMaybe (+) 0)
+      [ bench "drinkery" $ whnfIO $ drainD (D.scanningMaybe (+) 0)
       ]
   ]
