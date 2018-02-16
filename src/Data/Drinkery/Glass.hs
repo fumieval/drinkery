632978d769c9e250c6da0163ed52302f012a831b
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-02-16 14:23:34 +0900

    SimpleStill

--- src/Data/Drinkery/Glass.hs
+++ src/Data/Drinkery/Glass.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns, Rank2Types, FlexibleContexts #-}
 module Data.Drinkery.Glass where
 
 import Data.Drinkery.Class
@@ -7,7 +8,9 @@ import Data.Drinkery.Tap
 -- | Mono in/out
 type Still p q r s m = Distiller (Tap p (Maybe q)) r (Maybe s) m
 
-scan :: (Monoid r, Monad m) => (b -> a -> b) -> b -> Still r a r b m
+type SimpleStill a b m = forall r. Monoid r => Still r a r b m
+
+scan :: Monad m => (b -> a -> b) -> b -> SimpleStill a b m
 scan f b0 = consTap (Just b0) $ go b0 where
   go b = Tap $ \r -> Drinker $ \tap -> do
     (m, t') <- unTap tap r
@@ -16,10 +19,10 @@ scan f b0 = consTap (Just b0) $ go b0 where
       Nothing -> return ((Nothing, go b), t')
 {-# INLINE scan #-}
 
-map :: (Monoid r, Monad m) => (a -> b) -> Still r a r b m
+map :: Monad m => (a -> b) -> SimpleStill a b m
 map = mapping . fmap
 {-# INLINE map #-}
 
-filter :: (Monoid r, Monad m) => (a -> Bool) -> Still r a r a m
+filter :: Monad m => (a -> Bool) -> SimpleStill a a m
 filter = filtering . maybe True
 {-# INLINE filter #-}
