2d91ebb2ed9cc02808313acf203ec4e8a099a5a6
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-03-26 14:25:05 +0900

    reexport more

--- src/Data/Drinkery.hs
+++ src/Data/Drinkery.hs
@@ -1,11 +1,15 @@
 module Data.Drinkery
-    ( module Data.Drinkery.Class
+    ( module Data.Drinkery.Awaiter
+    , module Data.Drinkery.Class
     , module Data.Drinkery.Combinators
     , module Data.Drinkery.Distiller
+    , module Data.Drinkery.IO
     , module Data.Drinkery.Tap
-    )where
+    ) where
 
+import Data.Drinkery.Awaiter
 import Data.Drinkery.Class
 import Data.Drinkery.Combinators
 import Data.Drinkery.Distiller
+import Data.Drinkery.IO
 import Data.Drinkery.Tap
